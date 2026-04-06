b871f4f0dd3fd6cd8e08b91afb0d7b246d43dea1
SerkanToy <stoy@windowslive.com>
2026-04-06 14:22:59 +0300

    Domain katmanında Entity ve katmanın DI yapılacak kütüphanesi yapılandırıldı

---
+++ electronic.Domain/Entities/Abstractions/Entity.cs
@@ -0,0 +1,59 @@
+﻿using electronik.Domain.Entities.Users;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace electronic.Domain.Entities.Abstractions
+{
+    public abstract class Entity
+    {
+        public Entity()
+        {
+            Id = Guid.CreateVersion7();
+        }
+        public Guid Id { get; set; }
+
+        #region Audit Log
+        public bool IsActive { get; set; } = true;
+        public DateTimeOffset CreateAt { get; set; }
+        public Guid CreateUserId { get; set; } = default!;
+        public string CreateUserName => GetCreateUserName();
+        public DateTimeOffset? UpdateAt { get; set; }
+        public Guid? UpdateUserId { get; set; }
+        public string? UpdateUserName => GetUpdateUserName();
+        public bool IsDeleted { get; set; }
+        public DateTimeOffset? DeleteAt { get; set; }
+        public Guid? DeleteUserId { get; set; }
+        private string GetCreateUserName()
+        {
+            HttpContextAccessor httpContextAccessor = new();
+            var userManager = httpContextAccessor
+                .HttpContext
+                .RequestServices
+                .GetRequiredService<UserManager<UserApp>>();
+
+            UserApp appUser = userManager.Users.First(p => p.Id == CreateUserId);
+
+            return appUser.Name + " " + appUser.SurName + " (" + appUser.Email + ")";
+        }
+
+        private string? GetUpdateUserName()
+        {
+            if (UpdateUserId is null) return null;
+
+            HttpContextAccessor httpContextAccessor = new();
+            var userManager = httpContextAccessor
+                .HttpContext
+                .RequestServices
+                .GetRequiredService<UserManager<UserApp>>();
+
+            UserApp appUser = userManager.Users.First(p => p.Id == UpdateUserId);
+
+            return appUser.Name + " " + appUser.SurName + " (" + appUser.Email + ")";
+        }
+        #endregion
+    }
+}
---
+++ electronic.Domain/Entities/Employees/Address.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace electronic.Domain.Entities.Employees;
+
+
+public sealed record Address
+{
+    public string? Country { get; set; }
+    public string? City { get; set; }
+    public string? Town { get; set; }
+    public string? FulLAddress { get; set; }
+}
