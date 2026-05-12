af0c6d7383bfc76af87f0f8bad8fadf9913f9585
SerkanToy <stoy@windowslive.com>
2026-05-12 15:47:53 +0300

    AddressRepository oluşturuldu

---
+++ electronic.Application/Interface/IAddressRepository.cs
@@ -0,0 +1,12 @@
+﻿using electronic.Domain.Entities.Employees;
+using electronic.Domain.Entities.Employees.Address;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace electronic.Application.Interface
+{
+    public interface IAddressRepository : IGenericRepository<Address>
+    {
+    }
+}
---
+++ electronic.Infrastructure/Repositories/AddressRepository.cs
@@ -0,0 +1,13 @@
+﻿using electronic.Application.Interface;
+using electronic.Domain.Entities.Employees.Address;
+using electronic.Infrastructure.Context;
+
+namespace electronic.Infrastructure.Repositories
+{
+    public class AddressRepository : GenericRepository<Address>, IAddressRepository
+    {
+        public AddressRepository(CilingirogluDbContext _context) : base(_context)
+        {
+        }
+    }
+}
